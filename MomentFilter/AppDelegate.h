b87119421a4516a1f4f26385a5108c43c165e712
JulianYG <yg1811@hotmail.com>
2015-06-09 21:10:36 -0500

    Initial Commit

---
+++ MomentFilter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MomentFilter
+//
+//  Created by Gao Yuan on 6/9/15.
+//  Copyright (c) 2015 Purpl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MomentFilter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MomentFilter
+//
+//  Created by Gao Yuan on 6/9/15.
+//  Copyright (c) 2015 Purpl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
